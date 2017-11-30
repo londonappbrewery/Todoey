1917af66c3f816a584280e33acd868f9a713d60a
Angela <robots@londonappbrewery.com>
2017-11-30 11:16:50 +0000

    Persisting data using Codable protocol and Plist.

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -16,9 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
-
-        
         return true
     }
 
